46a8f24f6e84098233655a941013e8c33006d4b5
Andrey <andreyyershov424@gmail.com>
2023-01-10 13:42:49 +0200

    fix test

--- tests/todoMVC.spec.js
+++ tests/todoMVC.spec.js
@@ -5,7 +5,7 @@ test('create new todo', async ({ page }) => {
   await page.goto('https://todomvc.com/examples/react/#/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/todos/);
+  await expect(page).toHaveTitle(/React • TodoMVC/);
 });
 
 
