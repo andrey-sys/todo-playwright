bf222fd6eb84aea11736e56219159d8c61a027cc
Andrey <andreyyershov424@gmail.com>
2023-01-10 13:29:06 +0200

    test todomvc

---
+++ tests/todoMVC.spec.js
@@ -0,0 +1,11 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('create new todo', async ({ page }) => {
+  await page.goto('https://todomvc.com/examples/react/#/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/todos/);
+});
+
+
